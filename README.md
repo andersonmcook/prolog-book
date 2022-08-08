600c9f38b625fafca028f51c3431889e86e67835
Anderson Cook <andersonmcook@gmail.com>
2022-08-08 12:55:58 -0500

    Inferences

---
+++ README.md
@@ -0,0 +1,12 @@
+## Getting started
+
+1. Start SWI Prolog
+2. `consult(path_to_file)`
+
+## Syntax
+
+- `;` -> or
+- `,` -> and
+- `x` -> atom
+- `X` -> variable
+- `f(X)` -> fact
---
+++ twitter/db.pl
@@ -0,0 +1,30 @@
+% Facts
+
+tweep(jill).
+tweep(eric).
+tweep(jose).
+tweep(joe).
+tweep(anna).
+
+tweet(gossip).
+tweet(policy).
+tweet(declaration).
+tweet(greeting).
+tweet(screed).
+
+% Rules
+
+tweets(jill, greeting).
+tweets(anna, gossip).
+
+follows(eric, jill).
+follows(joe, anna).
+follows(eric, joe).
+follows(jill, joe).
+
+receives(Tweep, What) :-
+  % Tweep receives own tweet
+  tweets(Tweep, What);
+  % Otherwise they have to follow
+  tweets(Who, What),
+  follows(Tweep, Who).
