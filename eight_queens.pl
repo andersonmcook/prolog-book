60f01844ce8ea2ba68e7da62cbadb293ed0a5927
Anderson Cook <andersonmcook@gmail.com>
2022-08-12 15:19:51 -0500

    Eight Queens

--- eight_queens.pl
+++ eight_queens.pl
@@ -10,3 +10,24 @@ attack((R1, C1), (R2, C2)) :-
   abs(R1 - C1) =:= abs(R2 - C2).
 
 safe(QueenA, QueenB) :- \+ attack(QueenA, QueenB).
+
+% Solution
+
+queens(Queens) :-
+  length(Queens, 8),
+  Queens ins 1..8,
+  all_different(Queens),
+  safe(Queens).
+
+safe([]).
+safe([Queen|Queens]) :-
+  safe_queen(Queens, Queen, 1),
+  safe(Queens).
+
+safe_queen([], _, _).
+safe_queen([Queen|Queens], Target, Difference) :-
+  different_diagonals(Queen, Target, Difference),
+  safe_queen(Queens, Target, Difference + 1).
+
+different_diagonals(A, B, Distance) :-
+  abs(A - B) #\= Distance.
