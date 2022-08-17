83f6d302431da3e16305326edc40dd1a66a95f30
Anderson Cook <andersonmcook@gmail.com>
2022-08-17 12:54:12 -0500

    Weighted graphs

--- weighted.pl
+++ weighted.pl
@@ -11,3 +11,24 @@ edge(d, e, 2).
 connected(A, B, Weight) :-
   edge(A, B, Weight);
   edge(B, A, Weight).
+
+path(From, To, Path, Weight) :-
+  go(From, To, [From], Result, 0, Weight),
+  reverse(Path, Result).
+
+go(From, To, Seen, [To|Seen], WeightAcc, Weight) :-
+  connected(From, To, W),
+  Weight is WeightAcc + W.
+
+go(From, To, Seen, Path, WeightAcc, Weight) :-
+  connected(From, Between, W),
+  Between \== To,
+  \+member(Between, Seen),
+  NewWeight is WeightAcc + W,
+  go(Between, To, [Between|Seen], Path, NewWeight, Weight).
+
+minimum_path(From, To, Path, Weight) :-
+  aggregate(min(W, P), path(From, To, P, W), min(Weight, Path)).
+
+maximum_path(From, To, Path, Weight) :-
+  aggregate(max(W, P), path(From, To, P, W), max(Weight, Path)).
